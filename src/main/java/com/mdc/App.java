9e03d01e695be18cd101b852cad3f09bc5507110
Dragu Costel <dragu.costel@yahoo.com>
2017-11-22 20:27:56 +0200

    Initiate Maven project

---
+++ src/main/java/com/mdc/App.java
@@ -0,0 +1,11 @@
+package com.mdc;
+
+/**
+ * @author : Costel DRAGU
+ * @version $Id:$
+ * @package : com.mdc
+ * @copyright Copyright (c) 2017
+ * @date : 22.11.2017 20:26
+ */
+public class App {
+}
