e31d66814246668659b44af5b2bef2555e1397d6
Dragu Costel <dragu.costel@yahoo.com>
2017-11-22 21:32:35 +0200

    Add new that work

--- src/main/java/com/mdc/App.java
+++ src/main/java/com/mdc/App.java
@@ -1,5 +1,9 @@
 package com.mdc;
 
+import com.mdc.sequence.SequenceGenerator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 /**
  * @author : Costel DRAGU
  * @version $Id:$
@@ -8,4 +12,17 @@ package com.mdc;
  * @date : 22.11.2017 20:26
  */
 public class App {
+
+    private static final String CONTEXT_XML = "classpath:/META-INF/beans.xml";
+
+    public static void main(String[] args) {
+            ApplicationContext context =
+                    new GenericXmlApplicationContext(CONTEXT_XML);
+
+            SequenceGenerator generator =
+                    (SequenceGenerator) context.getBean("sequenceGenerator");
+
+            System.out.println(generator.getSequence());
+            System.out.println(generator.getSequence());
+        }
 }
